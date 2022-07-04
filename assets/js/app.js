eb8e0f831848fd76da084c81825e0255aa8ae917
Natan DAVID <natandav39@gmail.com>
2022-07-04 14:33:55 +0200

    second commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,19 @@
 var loader = document.querySelector("#fullloader");
+var jeu = document.querySelector("#jeu");
+var accueil = document.querySelector("#accueil");
+var starting = document.querySelector("#starting");
 
 window.onload = setTimeout(test, 3000);
 function test(){
     loader.style.display = "none";
+    accueil.style.display = "flex";
 };
+
+
+starting.addEventListener('click', function(){
+    accueil.classList.add("transcueil");
+    setTimeout(() => {
+        accueil.style.display = "none";
+        jeu.style.display = "flex";
+    }, 1000);
+})
