5616afda421bc17c1f25240a491dd8ac33210b76
Natan DAVID <natandav39@gmail.com>
2022-07-25 11:18:00 +0200

    Fin de projet

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,8 +19,17 @@ var fermeturefenetre = document.querySelector("#fermeturefenetre");
 let y = 0;
 var mention = {
     "title": "Mentions légales",
-    "content": "<div><h2>Editeur du Site :</h2><p class='fs-13 text-white'>Le site web <a href='natandavid.fr/projets/popcode'>natandavid.fr/popcode</a>est &eacute;dit&eacute; par les apprenants de l&#39;<a href='https://www.accesscodeschool.fr/' target='_blank'>Access Code School</a> de Lons le Saunier et n&#39;est en aucun cas destin&eacute; &agrave; une utilisation commerciale. Celui-ci &agrave; &eacute;t&eacute; d&eacute;veloppp&eacute; dans le cadre d&#39;un projet d&#39;&eacute;tude.</p><h2>H&eacute;bergement :</h2><p>OVH<br />SAS au capital de 200 000 euros<br />RCS Paris 393 078 647<br />Si&egrave;ge Sociale : 3 cit&eacute; Paradis 75010 Paris</p><h2>Cookies :</h2><p>Notre site internet utilise des cookies. Il s&rsquo;agit de petits fichiers texte enregistr&eacute;s sur votre disque dur. La plupart des cookies sont destin&eacute;s &agrave; permettre ou faciliter votre navigation et sont n&eacute;cessaires au fonctionnement du site. L&rsquo;utilisation de cookies, propres ou tiers, n&rsquo;&eacute;tant pas obligatoirement n&eacute;cessaires au fonctionnement du site n&eacute;cessite un consentement expr&egrave;s de votre part. Vous pouvez manifester votre consentement ou vous opposer &agrave; l&rsquo;utilisation des cookies en param&eacute;trant votre dispositif de connexion de mani&egrave;re appropri&eacute;e. Reportez-vous pour cela notamment au guide d&rsquo;utilisation de votre navigateur.</p><h2>Comment accepter, param&eacute;trer ou refuser un cookie ?</h2><p>Vous pouvez configurer votre navigateur de fa&ccedil;on &agrave; ce que les cookies soient enregistr&eacute;s ou rejet&eacute;s.</p><h2>Pour Microsoft Internet Explorer :</h2><ul><li>Choisissez le menu &nbsp;<strong>Outils</strong> &nbsp;puis &nbsp;<strong>Options Internet&nbsp;</strong></li><li>Cliquez sur l&rsquo;onglet &laquo; Confidentialit&eacute; &raquo;</li><li>S&eacute;lectionnez le niveau souhait&eacute; &agrave; l&rsquo;aide du curseur</li></ul><h2>Pour Mozilla Firefox :</h2><ul><li>Choisissez le menu <strong>Outils&nbsp;</strong>puis&nbsp;<strong>Options</strong></li><li>Cliquez sur l&rsquo;option <strong>Vie priv&eacute;e</strong></li></ul><h2>Pour Chrome :</h2><ul><li>Choisissez le menu <strong>Edition</strong> puis&nbsp;<strong>Pr&eacute;f&eacute;rences</strong>&nbsp;</li><li>Cliquez sur l&rsquo;option <strong>Donn&eacute;es personnelles</strong></li><li>Rubrique <strong>Cookies</strong>&nbsp;</li></ul><h2>Pour Safari :</h2><ul><li>Choisissez le menu <strong>Edition</strong> puis&nbsp;<strong>Pr&eacute;f&eacute;rences</strong>&nbsp;</li><li>Cliquez sur l&rsquo;option <strong>Donn&eacute;es personnelles</strong></li><li>Rubrique <strong>Cookies</strong>&nbsp;</li></ul><h2>Protection des donn&eacute;es personnelles :</h2><p>Aucune information personnelle n&rsquo;est collect&eacute;e &agrave; votre insu.<br />Aucune information personnelle n&rsquo;est c&eacute;d&eacute;e &agrave; des tiers.</p></div>"
+    "content": "<div><h2>Editeur du Site :</h2><p class='fs-13 text-white'>Le site web <a href='natandavid.fr/projets/popcode' target='_blank'>natandavid.fr/popcode</a>est &eacute;dit&eacute; par les apprenants de l&#39;<a href='https://www.accesscodeschool.fr/' target='_blank'>Access Code School</a> de Lons le Saunier et n&#39;est en aucun cas destin&eacute; &agrave; une utilisation commerciale. Celui-ci &agrave; &eacute;t&eacute; d&eacute;veloppp&eacute; dans le cadre d&#39;un projet d&#39;&eacute;tude.</p><h2>H&eacute;bergement :</h2><p>OVH<br />SAS au capital de 200 000 euros<br />RCS Paris 393 078 647<br />Si&egrave;ge Sociale : 3 cit&eacute; Paradis 75010 Paris</p><h2>Cookies :</h2><p>Notre site internet utilise des cookies. Il s&rsquo;agit de petits fichiers texte enregistr&eacute;s sur votre disque dur. La plupart des cookies sont destin&eacute;s &agrave; permettre ou faciliter votre navigation et sont n&eacute;cessaires au fonctionnement du site. L&rsquo;utilisation de cookies, propres ou tiers, n&rsquo;&eacute;tant pas obligatoirement n&eacute;cessaires au fonctionnement du site n&eacute;cessite un consentement expr&egrave;s de votre part. Vous pouvez manifester votre consentement ou vous opposer &agrave; l&rsquo;utilisation des cookies en param&eacute;trant votre dispositif de connexion de mani&egrave;re appropri&eacute;e. Reportez-vous pour cela notamment au guide d&rsquo;utilisation de votre navigateur.</p><h2>Comment accepter, param&eacute;trer ou refuser un cookie ?</h2><p>Vous pouvez configurer votre navigateur de fa&ccedil;on &agrave; ce que les cookies soient enregistr&eacute;s ou rejet&eacute;s.</p><h2>Pour Microsoft Internet Explorer :</h2><ul><li>Choisissez le menu &nbsp;<strong>Outils</strong> &nbsp;puis &nbsp;<strong>Options Internet&nbsp;</strong></li><li>Cliquez sur l&rsquo;onglet &laquo; Confidentialit&eacute; &raquo;</li><li>S&eacute;lectionnez le niveau souhait&eacute; &agrave; l&rsquo;aide du curseur</li></ul><h2>Pour Mozilla Firefox :</h2><ul><li>Choisissez le menu <strong>Outils&nbsp;</strong>puis&nbsp;<strong>Options</strong></li><li>Cliquez sur l&rsquo;option <strong>Vie priv&eacute;e</strong></li></ul><h2>Pour Chrome :</h2><ul><li>Choisissez le menu <strong>Edition</strong> puis&nbsp;<strong>Pr&eacute;f&eacute;rences</strong>&nbsp;</li><li>Cliquez sur l&rsquo;option <strong>Donn&eacute;es personnelles</strong></li><li>Rubrique <strong>Cookies</strong>&nbsp;</li></ul><h2>Pour Safari :</h2><ul><li>Choisissez le menu <strong>Edition</strong> puis&nbsp;<strong>Pr&eacute;f&eacute;rences</strong>&nbsp;</li><li>Cliquez sur l&rsquo;option <strong>Donn&eacute;es personnelles</strong></li><li>Rubrique <strong>Cookies</strong>&nbsp;</li></ul><h2>Protection des donn&eacute;es personnelles :</h2><p>Aucune information personnelle n&rsquo;est collect&eacute;e &agrave; votre insu.<br />Aucune information personnelle n&rsquo;est c&eacute;d&eacute;e &agrave; des tiers.</p></div>"
 }
+// modal règle
+var regle = document.querySelector(".regle");
+var fermregle = document.querySelector("#fermregle");
+var regleoverlay = document.querySelector(".regleoverlay");
+
+fermregle.addEventListener('click', function(){
+    regle.style.display = 'none';
+    regleoverlay.style.display = 'none';
+})
 
 
 window.onload = setTimeout(test, 3000);
@@ -123,36 +132,6 @@ async function openModal(element) {
             }
         })
 }
-
-// if (input.checked === true) {
-    
-// }
-
-// async function openModal(element) {
-//     const myRequest = new Request('assets/json/languages.json');
-//     fetch(myRequest)
-//         .then(response => response.json())
-//         .then(data => {
-//             for (let i = 0; i < data.length; i++) {
-//                 let test = data[i].name.toUpperCase();
-//                 if (element === test) {
-                    
-//                         document.querySelector('#titredelanguage').innerHTML = data[i].name; 
-//                         document.querySelector('#imagelanguage').src = data[i].picture; 
-//                         document.querySelector('#imagelanguage').alt = 'Logo ' + data[i].name; 
-//                         document.querySelector('#descriptionlanguage').innerHTML = data[i].description;
-                    
-//                     var modalclose = document.querySelector('.modalclose');
-//                     modalclose.addEventListener('click', function () {
-//                         modal.classList.remove('modalflex');
-//                     })
-//                     break
-//                 }
-
-//             }
-//         })
-// }
-
 window.addEventListener("keydown", function (e) {
     if (
         e.key === "a" ||
